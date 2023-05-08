f935a33292dd6a9aec84d905bcac2ecf6b5567c2
Jim Junker <james.junker1@gmail.com>
2023-05-08 18:53:04 -0500

    updated SEM analysis for paper

--- habitat-SEM.R
+++ habitat-SEM.R
@@ -2,82 +2,263 @@ library(tidyverse)
 library(lavaan)
 library(piecewiseSEM)
 library(mgcv)
+library(MASS)
+library(smwrStats)
+remotes::install_github("USGS-R/smwrStats")
 .pardefault <- par()
 
 ## load the data ----
 dat = read.csv("./data/divers_prod_hab.csv") %>% 
+  dplyr::mutate(production_sum_mean = (production_sum_mean/1000),
+                seg_weighted_size = (seg_weighted_size/10),
+                log_prod = log10(production_sum_mean)) %>% 
+  dplyr::mutate(prod_2 = production_sum_mean^2,
+                prod_3 = production_sum_mean^3,
+                habH_2 = shannon_habitat^2,
+                habH_3 = shannon_habitat^3,
+                is10 = median_nolog*10) %>% 
   dplyr::rename(prod = 'production_sum_mean', habH = 'shannon_habitat',
                 bioH_richness = 'RICHNESS', bioH_shannon = 'SHANNON_B',
                 bioH_simpson = 'SIMPSON_B', bioH_evenness = 'EVENNESS_B', is = 'median_nolog', sediment_size = 'seg_weighted_size') %>% 
   # scale the variables because production is on a vastly different scale than rest
-  dplyr::mutate(across(c(prod, habH,bioH_richness, bioH_shannon, bioH_simpson, bioH_evenness, sediment_size, is), ~as.numeric(scale(.x)), .names = "{.col}_scaled")) %>% 
-  dplyr::select(prod, prod_scaled, habH, habH_scaled, bioH_richness, bioH_richness_scaled, bioH_shannon, bioH_shannon_scaled, bioH_simpson, bioH_simpson_scaled, bioH_evenness, bioH_evenness_scaled,sediment_size, sediment_size_scaled, is, is_scaled)
+  dplyr::mutate(across(c(prod, prod_2, prod_3, habH, habH_2, habH_3, bioH_richness, bioH_shannon, bioH_simpson, bioH_evenness, sediment_size, is), ~as.numeric(scale(.x)), .names = "{.col}_scaled")) %>%
+  dplyr::mutate(d0 = bioH_richness,
+                d1 = exp(bioH_shannon),
+                d2 = 1/bioH_simpson) %>% 
+  dplyr::select(log_prod, prod,prod_2,prod_3, prod_scaled, prod_2_scaled, prod_3_scaled, habH, habH_2, habH_2_scaled, habH_3, habH_scaled, habH_2_scaled, bioH_richness, bioH_richness_scaled, bioH_shannon, bioH_shannon_scaled, bioH_simpson, bioH_simpson_scaled, bioH_evenness, bioH_evenness_scaled,sediment_size, sediment_size_scaled, is, is10, is_scaled, d0, d1, d2)
+
+dat_mod = dat %>%
+  dplyr::select(is,prod,bioH_shannon, bioH_simpson, bioH_richness, habH, sediment_size) %>% 
+  dplyr::mutate(across(everything(), ~.x + 0.001))
 
 # SEMs ----
 # fit with full path analysis/sems
-# read in the path analysis model syntax
-source("habitat-model-specs.R")
-## scaled SEMs ----
-### these fit fine
-fit_sem_richness_scaled <- sem(model_richness_scaled, data = dat, auto.var = TRUE)
-fit_sem_shannon_scaled <- sem(model_shannon_scaled, data = dat, auto.var = TRUE)
-fit_sem_simpson_scaled <- sem(model_simpson_scaled, data = dat, auto.var = TRUE)
-fit_sem_evenness_scaled <- sem(model_evenness_scaled, data = dat, auto.var = TRUE)
+mvnormtest::mshapiro.test(t(dat[,c('is_scaled','log_prod','bioH_shannon','habH','sediment_size')]))
+smwrStats::optimBoxCox(dat_mod[,c('is','prod','bioH_shannon','bioH_simpson','bioH_richness','habH','sediment_size')])
+dat_mod = dat_mod %>% 
+  dplyr::mutate(is = ((is^0.9)-1)/0.9,
+                prod = ((prod^-1.1)-1)/-1.1,
+                bioH_shannon = log(bioH_shannon),
+                bioH_simpson = ((bioH_simpson^0.9)-1)/0.9,
+                bioH_richness = ((bioH_richness^0.8)-1)/0.8,
+                habH = sqrt(habH),
+                sediment_size = ((sediment_size^0.3)-1)/0.3)
 
-# get the summaries for SEM fits
-summary(fit_sem_richness_scaled, fit.measures = TRUE, standardized = TRUE, rsquare = TRUE)
-summary(fit_sem_shannon_scaled, fit.measures = TRUE, standardized = TRUE, rsquare = TRUE)
-summary(fit_sem_simpson_scaled, fit.measures = TRUE, standardized = TRUE, rsquare = TRUE)
-summary(fit_sem_evenness_scaled, fit.measures = TRUE, standardized = TRUE, rsquare = TRUE)
-### models with latent variable for biodiversity (richness, shannon, evenness)
-# sem model 
-model_latentBio_scaled <-'
-# set out the model structure
+mvnormtest::mshapiro.test(t(dat_mod[,c('is','prod','bioH_shannon','bioH_simpson','bioH_richness','habH','sediment_size')]))
+model_sem_shannon <-'
 # the full model of interaction strengths
-# IS is influenced by production and diversity
+# is10 is IS * 10 to get variance on similar scales influenced by production and diversity
 
-is_scaled ~ prod_scaled + bioH_scaled
+is_scaled ~ log_prod + bioH_shannon
 
 # here prod itself has dependence on habitat Diversity
 # and biodiversity
 
-prod_scaled ~ habH_scaled + bioH_scaled+ sediment_size_scaled
+log_prod ~ habH + bioH_shannon + sediment_size
 
-# OTOH, biodiversity is influenced by habitat Diversity and sediment size
+# OTOH, biodiversity is_scaled influenced by habitat Diversity and sediment size
 
-bioH_scaled ~ habH_scaled + sediment_size_scaled
+bioH_shannon ~ habH + sediment_size
 
-sediment_size_scaled ~ habH_scaled
+sediment_size ~ habH
+'
+fit_sem_bioH_shannon <- sem(model_sem_shannon, data = dat, auto.var = TRUE)
+summary(fit_sem_bioH_shannon,fit.measures = TRUE, standardized = TRUE, rsquare = TRUE)
+standardizedsolution(fit_sem_bioH_shannon)
+
+fit_psem_bioH_shannon = psem(
+  lm(is10 ~ log_prod + bioH_shannon, dat),
+  lm(log_prod ~ habH + bioH_shannon + sediment_size, dat),
+  lm(bioH_shannon ~ habH + sediment_size, dat),
+  lm(sediment_size ~ habH, dat)
+)
+summary(fit_psem_bioH_shannon)
+AIC(fit_psem_bioH_shannon, AIC.type = "dsep")
+
+model_latentBio_simple <-'
+# the full model of interaction strengths
+# is_scaled is_scaled influenced by production and diversity
+
+is_scaled ~ log_prod + bioH
+
+# here prod itself has dependence on habitat Diversity
+# and biodiversity
+
+log_prod ~ habH + bioH + sediment_size
+
+# OTOH, biodiversity is_scaled influenced by habitat Diversity and sediment size
+
+bioH ~ habH + sediment_size
+
+sediment_size ~ habH
 
 ## identify the latent variables
 
-bioH_scaled =~ bioH_shannon_scaled + bioH_richness_scaled + bioH_evenness_scaled
+bioH =~ bioH_shannon_scaled + bioH_richness_scaled + bioH_simpson_scaled
+
+## covariances
+bioH_shannon_scaled ~~ bioH_richness_scaled
+bioH_richness_scaled ~~ bioH_simpson_scaled
 '
-fit_sem_bioH_scaled <- sem(model_latentBio_scaled, data = dat, auto.var = TRUE)
-summary(fit_sem_bioH_scaled,fit.measures = TRUE, standardized = TRUE, rsquare = TRUE)
-standardizedsolution(fit_sem_bioH_scaled)
+fit_sem_bioH_simple <- sem(model_latentBio_simple, data = dat, auto.var = TRUE)
+summary(fit_sem_bioH_simple, fit.measures = TRUE, standardized = TRUE, rsquare = TRUE)
+standardizedsolution(fit_sem_bioH_simple)
+### models with latent variable for biodiversity (richness, shannon, evenness)
+# production composite f
+dat_mod = dat[-which(dat$prod > 5),]
+is_prod_mod = lm(is_scaled ~ prod_scaled + prod_2_scaled, data = dat)
+qqnorm(resid(is_prod_mod));shapiro.test(resid(is_prod_mod))
+summary(is_prod_mod)$coefficients
+# prod beta: -0.092290891
+# prod_2 beta: 0.031447172
+# prod_3 beta: -0.003274193
+# prod_scaled beta: -0.10343031
+# prod_2_scaled beta: 0.17780430
+# prod_3_scaled beta: -0.09021509
+# is_scale~prod_scaled beta: -6.79282355
+# is_scale~prod_2_scaled beta: 12.87363157
+# is_scale~prod_3_scaled beta: -7.30716041
+prod_composite = -1.0343031 * dat$prod_scaled + 1.7780430 * dat$prod_2_scaled + -0.9021509 * dat$prod_3_scaled
 
-knitr::kable(standardizedsolution(fit_sem_bioH_scaled), format = 'pipe')
+## habitat composite
+prod_habH_mod = lm(prod_composite ~ habH_scaled + habH_2_scaled, data = data.frame(dat,prod_composite))
+plot(prod_habH_mod)
+shapiro.test(resid(prod_habH_mod))
+summary(prod_habH_mod)$coefficients
+# habH beta: -0.12291289
+# habH_2 beta: 0.08859962
+# habH_scaled beta: -0.032259388
+# habH_2_scaled beta: 0.018491807
+# is_scale~habH_scaled beta: -1.79139440
+#is_scale~habH_2_Scaled beta: 1.02686784
+habH_composite = -0.12684885 * dat$habH + 0.09386793 * dat$habH_2
 
-## do some weird stuff to estimate total effects
-# I am still working on perfecting this
-fit_sem_simpson_eff = sem(model_simpson_effects, data = dat, auto.var = TRUE)
+# more structure 
+model_latentBio <-'
+# the full model of interaction strengths
+# is_scaled is_scaled influenced by production and diversity
 
+is_scaled ~ prod_composite + bioH
 
-#effects summary
-summary(fit_sem_simpson_eff, fit.measures = TRUE, standardized = TRUE, rsquare = TRUE)
+# production has a non-linear response so we can create a composite variable
 
+prod_composite <~ -0.12232501 * prod_scaled + 0.23182805 * prod_2_scaled + -0.13158717 * prod_3_scaled
+
+# prod_composite <~ -6.79282355 * prod_scaled + 12.87363157 * prod_2_scaled + -7.30716041 * prod_3_scaled
+# habH has a non-linear response with production so another composite is necessary
+
+habH_composite <~ -0.032259388 * habH_scaled + 0.018491807 * habH_2_scaled
+
+# here prod itself has dependence on habitat Diversity
+# and biodiversity
+
+# prod_composite ~ habH_scaled + bioH + sediment_size
+prod_composite ~ habH_composite + bioH + sediment_size
+
+# OTOH, biodiversity is_scaled influenced by habitat Diversity and sediment size
+
+# bioH ~ habH_scaled + sediment_size
+bioH ~ habH_composite + sediment_size
+
+# sediment_size ~ habH_scaled
+sediment_size ~ habH_composite
+
+## identify the latent variables
+
+bioH =~ bioH_shannon_scaled + bioH_richness_scaled + bioH_evenness_scaled
+
+## covariances
+bioH_shannon_scaled ~~ bioH_richness_scaled
+bioH_shannon_scaled ~~ bioH_evenness_scaled
+bioH_richness_scaled ~~ bioH_evenness_scaled
+'
+fit_sem_bioH <- sem(model_latentBio, data = dat, auto.var = TRUE, fixed.x = FALSE)#, estimator = 'MLM', se = 'robust')
+varTable(fit_sem_bioH)
+modificationindices(fit_sem_bioH)
+summary(fit_sem_bioH,fit.measures = TRUE, standardized = TRUE, rsquare = TRUE)
+standardizedsolution(fit_sem_bioH)
 # quick look at AIC for comm diversity measures
 # really not that interesting just exploratory
 # next, fit different model structures 
-AIC(fit_sem_shannon_scaled, fit_sem_simpson_scaled, fit_sem_evenness_scaled, fit_sem_richness_scaled)
 AIC(fit_sem_bioH_scaled, fit_sem_bioH_scaled_2)
 
 # Get estimates of confidence intervals
-
-standardizedsolution(fit_sem_simpson_scaled)
 parameterestimates(fit_sem_bioH_scaled, standardized = TRUE)
 
+# piecewiseSEM ----
+# using piecewiseSEM to do SEM allowing for different data distributions!
+
+# Here it will make sense to look at the data and think about the data generation processes and error structures
+## is
+
+is_prod_mod = lm(is_scaled ~ prod_scaled + prod_2_scaled + prod_3_scaled, data = dat)
+summary(is_prod_mod)
+prod_composite = -5.744e+00 * dat$prod_scaled + 9.874e+00 * dat$prod_2_scaled + -5.010e+00 * dat$prod_3_scaled
+dat$prod_composite <- prod_composite
+
+
+## prod
+plot(dat$prod_composite, y = dat$is_scaled)
+plot(dat$prod_composite, x = (dat$prod))
+plot(dat$prod_composite, x = dat$sediment_size)# non-linear issues
+plot(dat$prod_composite, x = dat$habH)
+# check residuals from individual glms
+prod_lm = lm(prod_composite ~ habH + bioH_shannon + sediment_size, data = dat)
+shapiro.test(resid(prod_lm));hist(resid(prod_lm));summary(prod_lm);qqnorm(resid(prod_lm))
+
+## bioH
+plot(dat$bioH_shannon, x = dat$habH)
+plot(dat$bioH_shannon, x = dat$sediment_size)# non-linear issues
+# check residuals from individual glms
+bioH_lm = lm(bioH_shannon ~ habH + sediment_size, data = dat)
+shapiro.test(resid(bioH_lm));hist(resid(bioH_lm));summary(bioH_lm);qqnorm(resid(bioH_glm))
+
+# fitting piecewise models
+model_psem_shannon <- psem(
+  lm(is_scaled~ prod_composite + bioH_shannon_scaled,  data = dat),
+  lm(prod_composite ~ habH_scaled + bioH_shannon_scaled + sediment_size_scaled, data = dat),
+  lm(bioH_shannon_scaled ~ habH_scaled + sediment_size_scaled, data = dat),
+  lm(sediment_size_scaled ~ habH_scaled, data = dat)
+);
+basisSet(model_psem_shannon)
+dSep(model_psem_shannon)
+summary(model_psem_shannon)
+
+LLchisq(model_psem_shannon)
+
+# fitting simpson
+is_prod_mod = lm(is_scaled ~ prod_scaled + prod_2_scaled + prod_3_scaled, data = dat)
+summary(is_prod_mod)
+prod_composite = -5.744e+00 * dat$prod_scaled + 9.874e+00 * dat$prod_2_scaled + -5.010e+00 * dat$prod_3_scaled
+dat$prod_composite <- prod_composite
+
+
+## prod
+plot(dat$prod_composite, y = dat$is_scaled)
+plot(dat$prod_composite, x = (dat$prod))
+plot(dat$prod_composite, x = dat$sediment_size)# non-linear issues
+plot(dat$prod_composite, x = dat$habH)
+# check residuals from individual glms
+prod_lm = lm(prod_composite ~ habH + bioH_simpson + sediment_size, data = dat)
+shapiro.test(resid(prod_lm));hist(resid(prod_lm));summary(prod_lm);qqnorm(resid(prod_lm))
+
+## bioH
+plot(dat$bioH_simpson, x = dat$habH)
+plot(dat$bioH_simpson, x = dat$sediment_size)# non-linear issues
+# check residuals from individual glms
+bioH_lm = lm(bioH_simpson ~ habH + sediment_size, data = dat)
+shapiro.test(resid(bioH_lm));hist(resid(bioH_lm));summary(bioH_lm);qqnorm(resid(bioH_glm))
+
+# fitting piecewise models
+model_psem_simpson <- psem(
+  lm(is_scaled~ prod_composite + bioH_simpson,  data = dat),
+  lm(prod_composite ~ habH + bioH_simpson + sediment_size, data = dat),
+  lm(bioH_simpson ~ habH + sediment_size, data = dat),
+  lm(sediment_size ~ habH, data = dat)
+);
+basisSet(model_psem_simpson)
+dSep(model_psem_simpson)
+summary(model_psem_simpson)
 # Spare(d) code ----
 ## confirmatory factor analysis ----
 ## richness
@@ -117,76 +298,7 @@ summary(fit_cfa_simpson, fit.measures = TRUE, standardized = TRUE, rsquare = TRU
 summary(fit_cfa_evenness, fit.measures = TRUE, standardized = TRUE, rsquare = TRUE)
 
 
-# piecewiseSEM ----
-# using piecewiseSEM to do CFA allowing for different data distributions!
 
-# Here it will make sense to look at the data and think about the data generation processes and error structures
-plot(dat$is_scaled, dat$bioH_shannon_scaled)
-plot(dat$is_scaled, x = dat$prod_scaled)# non-linear issues
-plot(dat$habH_scaled, dat$bioH_shannon_scaled)
-# plot(lm(bioH_shannon_scaled ~ habH_scaled, data = dat))
-
-# check residuals from individual glms
-plot(glm(is_scaled ~ prod_scaled + bioH_shannon_scaled, family = gaussian(link = 'identity'), data = dat))
-
-summary(glm(is_scaled ~ prod_scaled + bioH_shannon_scaled, family = Gamma(), data = dat))
-
-
-
-# fitting piecewise models
-model_psem_shannon_scaled <- psem(
-  gam(is_scaled ~ s(prod_scaled) + bioH_shannon_scaled, data = dat),
-  gam(prod_scaled ~ s(habH_scaled) + s(bioH_shannon_scaled) + sediment_size_scaled,  data = dat),
-  glm(bioH_shannon_scaled ~ habH_scaled+ sediment_size_scaled, data = dat),
-  data = dat);
-
-model_psem_shannon_scaled
-
-
-summary(model_psem_shannon_scaled)
-
-AIC(model_psem_shannon,model_psem_shannon_scaled)
-
-
-
-summary(model_psem_shannon_scaled)
-
-model_psem_simpson_scaled <- psem(
-  glm(is_scaled ~ prod_scaled + bioH_simpson_scaled, family = gaussian(link = "identity"), data = dat),
-  glm(prod_scaled ~ bioH_simpson_scaled + habH_scaled+ sediment_size_scaled, family = gaussian(link = "identity"), data = dat),
-  glm(bioH_simpson_scaled ~ habH_scaled + sediment_size_scaled, family = gaussian(link = 'identity'), data = dat),
-  glm(sediment_size_scaled ~ habH_scaled, family = gaussian(link = 'identity'), data = dat),
-  data = dat)
-
-summary(model_psem_simpson_scaled)
-dSep(model_psem_simpson_scaled)
-# all plotting of these are meh
-plot(model_psem_simpson_scaled)
-
-
-## playing with other model structures e.g., GAMs
-model_psem_simpson_scaled_gam <- psem(
-  gam(is_scaled ~ s(prod_scaled) + s(bioH_simpson_scaled), family = gaussian(link = "identity"), data = dat),
-  gam(prod_scaled ~ s(bioH_simpson_scaled) + s(habH_scaled), family = gaussian(link = "identity"), data = dat),
-  gam(bioH_simpson_scaled ~ s(habH_scaled) + s(sediment_size_scaled), family = gaussian(link = "identity"),data = dat),
-  data = dat)
-
-summary(model_psem_simpson_scaled_gam)
-
-model_psem_evenness_scaled <- psem(
-  glm(is_scaled ~ prod_scaled + bioH_evenness_scaled, family = gaussian(link = "identity"), data = dat),
-  glm(prod_scaled ~ habH_scaled + bioH_evenness_scaled + sediment_size_scaled, family = gaussian(link = "identity"), data = dat),
-  glm(bioH_evenness_scaled ~ habH_scaled + sediment_size_scaled, family = gaussian(link = "identity"), data = dat),
-  data = dat)
-summary(model_psem_evenness_scaled)
-
-model_psem_richness_scaled <- psem(
-  glm(is_scaled ~ prod_scaled + bioH_richness_scaled, family = gaussian(link = "identity"), data = dat),
-  glm(prod_scaled ~ habH_scaled + bioH_richness_scaled, family = gaussian(link = "identity"), data = dat),
-  lm(bioH_richness_scaled ~ habH_scaled, data = dat),
-  data = dat)
-dSep(model_psem_simpson_scaled)
-plot(model_psem_simpson_scaled)
 
 
 ## attempts to visualize...they all pretty much suck
@@ -201,6 +313,33 @@ semPaths(
 
 
 
+# read in the path analysis model syntax
+source("habitat-model-specs.R")
+## scaled SEMs ----
+### these converge fine, but still show "poor" model fits CFI < 0.9
+fit_sem_richness <- sem(model_richness, data = dat, auto.var = TRUE)
+varTable(fit_sem_richness)
+fit_sem_shannon <- sem(model_shannon, data = dat, auto.var = TRUE)
+fit_sem_simpson <- sem(model_simpson, data = dat, auto.var = TRUE)
+fit_sem_evenness <- sem(model_evenness, data = dat, auto.var = TRUE)
+varTable(fit_sem_evenness)
+# get the summaries for SEM fits
+summary(fit_sem_richness, fit.measures = TRUE, standardized = TRUE, rsquare = TRUE)
+summary(fit_sem_shannon, fit.measures = TRUE, standardized = TRUE, rsquare = TRUE)
+summary(fit_sem_simpson, fit.measures = TRUE, standardized = TRUE, rsquare = TRUE)
+summary(fit_sem_evenness_scaled, fit.measures = TRUE, standardized = TRUE, rsquare = TRUE)
+
+## modification indices
+modificationindices(fit_sem_shannon)
+# lhs           op    rhs       mi      epc     sepc.lv sepc.all sepc.nox
+# is_scaled     ~ sediment_size  2.629  0.257   0.257    0.132    0.132
+# is_scaled     ~          habH 74.103 -3.054  -3.054   -0.777   -3.070
+# prod          ~     is_scaled 62.835  1.037   1.037    0.925    0.925
+# bioH_shannon  ~     is_scaled 10.811 -0.171  -0.171   -0.500   -0.500
+# sediment_size ~     is_scaled 11.062  0.821   0.821    1.596    1.596
+# habH          ~     is_scaled 74.103 -1.258  -1.258   -4.945   -4.945
+
+## just focus on Shannon index and add in options to make linear with CFA
 
 
 
--- habitat-model-specs.R
+++ habitat-model-specs.R
@@ -1,145 +1,173 @@
 # scaled path models with single biodiversity measures
 ## scaled models ----
-model_shannon_scaled <-'
+model_shannon <-'
 # set out the model structure
 # the full model of interaction strengths
-# IS is influenced by production and diversity
+# is_scaled is_scaled influenced by production and diversity
 
-is_scaled ~ 1+prod_scaled + bioH_shannon_scaled
+is_scaled ~ 1+prod + bioH_shannon
 
 # here prod itself has dependence on habitat Diversity
 # and biodiversity
 
-prod_scaled ~ 1+habH_scaled + bioH_shannon_scaled+ sediment_size_scaled
+prod ~ 1+habH + bioH_shannon+ sediment_size
 
-# OTOH, biodiversity is influenced by habitat Diversity and sediment size
+# OTOH, biodiversity is_scaled influenced by habitat Diversity and sediment size
 
-bioH_shannon_scaled ~ 1+habH_scaled + sediment_size_scaled
+bioH_shannon ~ 1+habH + sediment_size
 
-sediment_size_scaled ~ 1+habH_scaled
+sediment_size ~ 1+habH
 '
 
-model_simpson_scaled <-'
+model_simpson <-'
 # set out the model structure
 # the full model of interaction strengths
-# IS is influenced by production and diversity
+# is_scaled is_scaled influenced by production and diversity
 
-is_scaled ~ 1+prod_scaled + bioH_simpson_scaled
+is_scaled ~ 1+prod + bioH_simpson
 
 # here prod itself has dependence on habitat Diversity
 # and biodiversity
 
-prod_scaled ~ 1+habH_scaled + bioH_simpson_scaled+ sediment_size_scaled
+prod ~ 1+habH + bioH_simpson+ sediment_size
 
-# OTOH, biodiversity is influenced by habitat Diversity
+# OTOH, biodiversity is_scaled influenced by habitat Diversity
 
-bioH_simpson_scaled ~ 1+habH_scaled+ sediment_size_scaled
+bioH_simpson ~ 1+habH+ sediment_size
 
-sediment_size_scaled ~ 1+habH_scaled
+sediment_size ~ 1+habH
 '
 
-model_evenness_scaled <-'
+model_evenness <-'
 # set out the model structure
 # the full model of interaction strengths
-# IS is influenced by production and diversity
+# is_scaled is_scaled influenced by production and diversity
 
-is_scaled ~ 1+prod_scaled + bioH_evenness_scaled
+is_scaled ~ 1+prod + bioH_evenness_scaled
 
 # here prod itself has dependence on habitat Diversity
 # and biodiversity
 
-prod_scaled ~ 1+habH_scaled + bioH_evenness_scaled+ sediment_size_scaled
+prod ~ 1+habH + bioH_evenness_scaled+ sediment_size
 
-# OTOH, biodiversity is only influenced by habitat Diversity
+# OTOH, biodiversity is_scaled only influenced by habitat Diversity
 
-bioH_evenness_scaled ~ 1+habH_scaled+ sediment_size_scaled
+bioH_evenness_scaled ~ 1+habH+ sediment_size
 
-sediment_size_scaled ~ 1+habH_scaled
+sediment_size ~ 1+habH
 '
 
-model_richness_scaled <-'
+model_richness <-'
 # set out the model structure
 # the full model of interaction strengths
-# IS is influenced by production and diversity
+# is_scaled is_scaled influenced by production and diversity
 
-is_scaled ~ 1+prod_scaled + bioH_richness_scaled
+is_scaled ~ 1+prod + bioH_richness_scaled
 
 # here prod itself has dependence on habitat Diversity
 # and biodiversity
 
-prod_scaled ~ 1+habH_scaled + bioH_richness_scaled+ sediment_size_scaled
+prod ~ 1+habH + bioH_richness_scaled+ sediment_size
 
-# OTOH, biodiversity is only influenced by habitat Diversity
+# OTOH, biodiversity is_scaled only influenced by habitat Diversity
 
-bioH_richness_scaled ~ 1+habH_scaled+ sediment_size_scaled
+bioH_richness_scaled ~ 1+habH+ sediment_size
 
-sediment_size_scaled ~ 1+habH_scaled
+sediment_size ~ 1+habH
 '
 
 ## SEM with latent variables for biodiversity
-model_latentBio_scaled <-'
+
+# exercis_scalede to determine the structure of model
+model_latentBio_min <- '
 # set out the model structure
 # the full model of interaction strengths
-# IS is influenced by production and diversity
+# is_scaled is_scaled influenced by production and diversity
 
-is_scaled ~ 1+prod_scaled + bioH_scaled
+is_scaled ~ prod + bioH
+
+# here prod itself has dependence on habitat Diversity + sediment size
+
+prod ~ habH + sediment_size
+
+# OTOH, biodiversity is_scaled influenced by habitat Diversity and sediment size
+
+bioH ~ habH + sediment_size
+
+sediment_size ~ habH
+
+## identify the latent variables
+
+bioH =~ bioH_shannon + bioH_richness_scaled + bioH_evenness_scaled
+'
+model_latentBio <-'
+# set out the model structure
+# the full model of interaction strengths
+# is_scaled is_scaled influenced by production and diversity
+
+is ~ log_prod + bioH
 
 # here prod itself has dependence on habitat Diversity
 # and biodiversity
 
-prod_scaled ~ 1+habH_scaled + bioH_scaled+ sediment_size_scaled
+log_prod ~ habH + bioH + sediment_size
 
-# OTOH, biodiversity is influenced by habitat Diversity and sediment size
+# OTOH, biodiversity is_scaled influenced by habitat Diversity and sediment size
 
-bioH_scaled ~ habH_scaled + sediment_size_scaled
+bioH ~ habH + sediment_size
 
-sediment_size_scaled ~ habH_scaled
+sediment_size ~ habH
 
 ## identify the latent variables
 
-bioH_scaled =~ bioH_shannon_scaled + bioH_richness_scaled + bioH_evenness_scaled
+bioH =~ bioH_shannon + bioH_richness_scaled + bioH_evenness_scaled
+
+## covariances
+bioH_shannon ~~ bioH_richness_scaled
+bioH_shannon ~~ bioH_evenness_scaled
+bioH_richness_scaled ~~ bioH_evenness_scaled
+
 '
 
-model_latentBio_scaled_2 <-'
+model_latentBio_2 <-'
 # set out the model structure
 # the full model of interaction strengths
-# IS is influenced by production and diversity
+# is_scaled is_scaled influenced by production and diversity
 
-is_scaled ~ prod_scaled + bioH_scaled
+is_scaled ~ 1+prod + bioH
 
 # here prod itself has dependence on habitat Diversity
 # and biodiversity
 
-prod_scaled ~ habH_scaled + sediment_size_scaled
+prod ~ 1+habH + bioH + sediment_size
 
-# OTOH, biodiversity is influenced by habitat Diversity and sediment size
+# OTOH, biodiversity is_scaled influenced by habitat Diversity and sediment size
 
-bioH_scaled ~ habH_scaled + + prod_scaled + sediment_size_scaled
+bioH ~ habH + sediment_size
 
-sediment_size_scaled ~ habH_scaled
+sediment_size ~ habH
 
 ## identify the latent variables
 
-bioH_scaled =~ bioH_shannon_scaled + bioH_richness_scaled + bioH_evenness_scaled
+bioH =~ bioH_shannon + bioH_richness_scaled + bioH_evenness_scaled
 '
 
- 
 ## effects models ----
 model_simpson_effects <-'
 # set out the model structure
 # the full model of interaction strengths
-# IS is influenced by production and diversity
+# is_scaled is_scaled influenced by production and diversity
 
-is ~ b * prod_scaled + d * bioH_simpson_scaled
+is_scaled ~ b * prod + d * bioH_simpson
 
 # here prod itself has dependence on habitat Diversity
 # and biodiversity
 
-prod ~ a * habH_scaled + e * bioH_simpson_scaled+ sediment_size_scaled
+prod ~ a * habH + e * bioH_simpson+ sediment_size
 
-# OTOH, biodiversity is only influenced by habitat Diversity
+# OTOH, biodiversity is_scaled only influenced by habitat Diversity
 
-bioH_simpson ~ c * habH_scaled + f * sediment_size_scaled
+bioH_simpson ~ c * habH + f * sediment_size
 
 # calculating the effects of habitat diversity through biodiversity
 habH_TIE := a*b*c*d*e
